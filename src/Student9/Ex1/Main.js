2b964a8f3d3da44dd7377f0e9845a41e08ea7c32
Melissa Schaefer <mschaefer@xpanxion.com>
2023-05-23 11:06:42 -0400

    update with ex1

--- src/Student9/Ex1/Main.js
+++ src/Student9/Ex1/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Main.css'
+import { DateFetcher } from './DateFetcher'
+
 
 export const Main = () => {
+
   return (
     <div>
-      TODO...
+      <DateFetcher />
     </div>
   )
 }
