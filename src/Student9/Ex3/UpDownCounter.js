ad28de07e211a687781caee5e110965ba3d63585
Melissa Schaefer <mschaefer@xpanxion.com>
2023-05-24 17:04:30 -0400

    Add Exercices 2-3-4-5-6

---
+++ src/Student9/Ex3/UpDownCounter.js
@@ -0,0 +1,23 @@
+import React,{useState} from "react";
+
+
+export const UpDownCounter =()=>{
+    let [counter,setCounter]=useState(0)
+
+    const onhandleClick =(direction)=>{
+            if(direction==="Up"){
+                setCounter(counter++)
+            }else if(direction==="down"){
+                setCounter(counter--)
+            }
+
+    }
+    //return
+    return(
+        <div>
+            <button onClick={()=>onhandleClick("Up")} >Up</button>
+            {counter}
+            <button onClick={()=>onhandleClick("down")}>Down</button>
+        </div>
+    )
+}
